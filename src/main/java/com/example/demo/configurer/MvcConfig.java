476efdae86d71eac67d7306710fc45576a8a3e6f
gj <864350301@qq.com>
2019-01-04 11:38:50 +0800

    first commit init project

---
+++ src/main/java/com/example/demo/configurer/MvcConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.configurer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 功能描述:
+ * 模块:
+ * 项目:
+ * 版本号:V1.0
+ * 部门:技术研发部
+ * 公司:浙江乐融融科技有限公司
+ * 作者:郭军
+ * 邮箱:864350301@qq.com
+ * 创建时间:2019/1/2
+ * ************************************
+ * ************************************
+ * 修改人:
+ * 修改时间:
+ * 修改内容:
+ * 1.
+ * 2.
+ */
+@SpringBootApplication
+@ConditionalOnClass(MvcConfig.class)
+public class MvcConfig  implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+}
---
+++ src/main/java/com/example/demo/support/AjaxResult.java
@@ -0,0 +1,49 @@
+package com.example.demo.support;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 功能描述:
+ * 模块:
+ * 项目:
+ * 版本号:V1.0
+ * 部门:技术研发部
+ * 公司:浙江乐融融科技有限公司
+ * 作者:郭军
+ * 邮箱:864350301@qq.com
+ * 创建时间:2019/1/3
+ * ************************************
+ * ************************************
+ * 修改人:
+ * 修改时间:
+ * 修改内容:
+ * 1.
+ * 2.
+ */
+@ApiModel(value = "ajaxResult",description = "统一返回对象")
+@Data
+public class AjaxResult<E> implements Serializable {
+
+	public static final int SUCCESS = 1;
+	public static final int FAILURE = -1;
+
+	private static final long serialVersionUID = 5576237395711742681L;
+
+	@ApiModelProperty(name = "success",value = "成功失败标志",required = false)
+	private boolean success = false;
+
+	@ApiModelProperty(name = "errorCode",value = "错误代码",required = false)
+	private String errorCode;
+
+	@ApiModelProperty(name = "msg",value = "错误消息提示",required = false)
+	private String msg = "";
+
+	@ApiModelProperty(name = "data",value = "实际返回数据",required = false)
+	private E data = null;
+
+
+}
